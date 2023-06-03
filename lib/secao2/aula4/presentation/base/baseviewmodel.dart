01e198c0cbcd545ae6a1c2e020a73054aeb5b997
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2023-06-03 13:26:22 -0300

    Aula 34

---
+++ lib/secao2/aula4/presentation/base/baseviewmodel.dart
@@ -0,0 +1,15 @@
+abstract class BaseViewModel{
+
+  // shared variables and functions that will be used through any view model.
+
+
+}
+
+abstract class BaseViewModelInput{
+  void start(); // will be called while init. of view model
+  void dispose(); // will be called when viewmodel dies.
+}
+
+abstract class BaseViewModelOutput{
+
+}
