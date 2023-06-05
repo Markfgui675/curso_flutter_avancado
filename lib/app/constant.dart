9304e48dddbad07fd74df70025efc71cdddd5ad6
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-06-05 17:37:58 -0300

    Aula 47

---
+++ lib/app/constant.dart
@@ -0,0 +1,5 @@
+class Constant{
+
+  static const String baseUrl = 'https://markfgui.wiremockapi.cloud/';
+
+}
---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,15 @@
+import 'package:retrofit/http.dart';
+import 'package:dio/dio.dart';
+import '../../app/constant.dart';
+import '../responses/responses.dart';
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constant.baseUrl)
+abstract class AppServiceClient{
+
+  factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+
+  @POST("/customers/login")
+  Future<AuthenticationResponse> login();
+
+}
