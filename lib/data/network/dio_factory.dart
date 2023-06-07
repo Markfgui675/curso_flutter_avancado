4becac6977be0e7ba501642c3448d40ce3f5e615
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-06-07 17:22:12 -0300

    Aula 63

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
-
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import '../../app/constant.dart';
 
 const String APPLICATION_JSON = "application/json";
@@ -27,6 +28,16 @@ class DioFactory{
       receiveTimeout: _timeOut,
       headers: headers
     );
+
+    if(kReleaseMode){
+      print("release mode no logs");
+    }else{
+      dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseHeader: true,
+      ));
+    }
 
     return dio;
   }
