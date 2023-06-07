7d903bd0b41ffc1c2845f64519b07d91c873ebca
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-06-07 16:34:29 -0300

    Aula 60

--- lib/data/repository/error_handler.dart
+++ lib/data/repository/error_handler.dart
@@ -1,5 +1,6 @@
 import 'package:curso_flutter_avancado/data/network/failure.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 enum DataSource{
   SUCCESS,
@@ -14,7 +15,59 @@ enum DataSource{
   RECEIVE_TIMEOUT,
   SEND_TIMEOUT,
   CHACHE_ERROR,
+  DEFAULT,
   NI_INTERNNET_CONNECTION
+}
+
+class ErrorHandler implements Exception{
+
+  late Failure failure;
+
+  ErrorHandler.handler(dynamic error){
+    if(error is DioError){
+      // dio error so its error from response of the API
+      failure = _handleError(error);
+    } else {
+      // default error
+      failure = DataSource.DEFAULT.getFailure();
+    }
+  }
+
+  Failure _handleError(DioError error){
+    switch(error.type){
+      case DioErrorType.connectTimeout:
+        return DataSource.CONNECT_TIMEOUT.getFailure();
+
+      case DioErrorType.sendTimeout:
+        return DataSource.SEND_TIMEOUT.getFailure();
+
+      case DioErrorType.receiveTimeout:
+        return DataSource.RECEIVE_TIMEOUT.getFailure();
+
+      case DioErrorType.response:
+        switch(error.response!.statusCode){
+          case ResponseCode.BAD_REQUEST:
+            return DataSource.BAD_REQUEST.getFailure();
+          case ResponseCode.FORBIDDEN:
+            return DataSource.FORBIDDEN.getFailure();
+          case ResponseCode.UNAUTHORISED:
+            return DataSource.UNAUTHORISED.getFailure();
+          case ResponseCode.NOT_FOUND:
+            return DataSource.NOT_FOUND.getFailure();
+          case ResponseCode.INTERNAL_SERVER_ERROR:
+            return DataSource.INTERNAL_SERVER_ERROR.getFailure();
+          default:
+            return DataSource.DEFAULT.getFailure();
+        }
+
+      case DioErrorType.cancel:
+        return DataSource.CANCEL.getFailure();
+
+      case DioErrorType.other:
+        return DataSource.DEFAULT.getFailure();
+    }
+  }
+
 }
 
 class ResponseCode{
