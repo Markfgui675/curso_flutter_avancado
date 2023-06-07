29d9df9a145f717a6668c8e63bea7962c0a351bc
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-06-07 15:51:25 -0300

    Aula 58

--- lib/data/repository/error_handler.dart
+++ lib/data/repository/error_handler.dart
@@ -33,3 +33,23 @@ class ResponseCode{
   static const int CHACHE_ERROR =  -6;
   static const int NI_INTERNNET_CONNECTION = -6;
 }
+
+class ResponseMessage{
+  // API status codes
+  static const String SUCCESS = "succes";
+  static const String NO_CONTENT = "success with no data"; //success with no data
+  static const String BAD_REQUEST = "Bad request, try again later"; //failure, api rejected the request
+  static const String FORBIDDEN = "Forbidden request, try again later"; //failure, api rejected the request
+  static const String UNAUTHORISED = "Usr is anauthorised, try again later"; // failure user is not authorised
+  static const String NOT_FOUND = "Url is not found, try again later"; // failure, api url is not correct
+  static const String INTERNAL_SERVER_ERROR = "Something went wrong, try again later"; //failure, crash happened in server side
+
+  // local status code
+  static const String UNKOWN = "Something went wrong, try again later";
+  static const String CONNECT_TIMEOUT = "Time out error, try again later";
+  static const String CANCEL = "Request was cancelled, try again later";
+  static const String RECEIVE_TIMEOUT =  "time out error, try again later";
+  static const String SEND_TIMEOUT = "Time out error, try again later";
+  static const String CHACHE_ERROR =  "Cache error, try again later";
+  static const String NI_INTERNNET_CONNECTION = "Please check your internet connection";
+}
