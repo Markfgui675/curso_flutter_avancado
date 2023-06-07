c8baadd4c662ce3bb6123a024fbd3da01c9ef854
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-06-07 15:42:43 -0300

    Aula 57

--- lib/data/repository/error_handler.dart
+++ lib/data/repository/error_handler.dart
@@ -8,8 +8,28 @@ enum DataSource{
   INTERNAL_SERVER_ERROR,
   CONNECT_TIMEOUT,
   CANCEL,
-  RECIEVE_TIMEOUT,
+  RECEIVE_TIMEOUT,
   SEND_TIMEOUT,
   CHACHE_ERROR,
   NI_INTERNNET_CONNECTION
+}
+
+class ResponseCode{
+  // API status codes
+  static const int SUCCESS = 200;
+  static const int NO_CONTENT = 201; //success with no data
+  static const int BAD_REQUEST = 400; //failure, api rejected the request
+  static const int FORBIDDEN = 403; //failure, api rejected the request
+  static const int UNAUTHORISED = 401; // failure user is not authorised
+  static const int NOT_FOUND = 404; // failure, api url is not correct
+  static const int INTERNAL_SERVER_ERROR = 500; //failure, crash happened in server side
+
+  // local status code
+  static const int UNKOWN = -1;
+  static const int CONNECT_TIMEOUT = -2;
+  static const int CANCEL = -3;
+  static const int RECEIVE_TIMEOUT =  -4;
+  static const int SEND_TIMEOUT = -5;
+  static const int CHACHE_ERROR =  -6;
+  static const int NI_INTERNNET_CONNECTION = -6;
 }
