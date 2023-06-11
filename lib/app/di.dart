0d26a20c770b0a62d2cf40752185bc2f8818ddc2
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2023-06-11 18:38:52 -0300

    Aula 84

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,6 +1,15 @@
 import 'package:curso_flutter_avancado/app/app_preferences.dart';
+import 'package:curso_flutter_avancado/data/network/network_info.dart';
+import 'package:curso_flutter_avancado/data/repository/repository_impl.dart';
+import 'package:curso_flutter_avancado/domain/repository/repository.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import '../data/data_source/remote_data_source.dart';
+import '../data/network/app_api.dart';
+import '../data/network/dio_factory.dart';
 
 
 // di -> dependency injection
@@ -10,12 +19,29 @@ final instance = GetIt.instance;
 
 Future<void> initAppModule() async {
 
-  final sharedPreferences = SharedPreferences.getInstance();
+  final sharedPreferences = await SharedPreferences.getInstance();
 
   // shred preferences instance
-  instance.registerLazySingleton(() => sharedPreferences);
+  instance.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
 
   // app prefs instance
   instance.registerLazySingleton<AppPreferences>(() => AppPreferences(instance()));
+
+  // network info
+  instance.registerLazySingleton<NetworkInfo>(() => NetworkInfoImplement(DataConnectionChecker()));
+
+  // dio factory
+  instance.registerLazySingleton<DioFactory>(() => DioFactory(instance()));
+
+  // app service client
+  final dio = await instance<DioFactory>().getDio();
+  instance.registerLazySingleton<AppServiceClient>(() => AppServiceClient(dio));
+
+  // remote data source
+  instance.registerLazySingleton<RemoteDataSource>(() => RemoteDataSourceImplementer(instance()));
+
+  // repository
+  instance.registerLazySingleton<Repository>(() => RepositoryImpl(instance(), instance()));
+
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:curso_flutter_avancado/app/app.dart';
+import 'package:curso_flutter_avancado/app/di.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initAppModule();
   runApp(MyApp());
 }
