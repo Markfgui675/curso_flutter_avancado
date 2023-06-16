d61a967763062cd21ff2b87f5b00a4b6677d6116
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-06-16 15:53:38 -0300

    Aula 122

--- lib/presentation/register/register_viewmodel.dart
+++ lib/presentation/register/register_viewmodel.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'dart:html';
 import '../../domain/usecase/register_usecase.dart';
 import '../base/baseviewmodel.dart';
 
@@ -30,5 +30,32 @@ class RegisterViewModel extends BaseViewModel{
     _isAllInputsValidStreamController.close();
     super.dispose();
   }
+
+}
+
+abstract class RegisterViewModelInput{
+  register();
+
+  Sink get inputUserName;
+  Sink get inputMobileNumber;
+  Sink get inputEmail;
+  Sink get inputPassword;
+  Sink get inputProfilePicture;
+}
+abstract class RegisterViewModelOutput{
+  Stream<bool> get outputIsUserNameValid;
+  Stream<String> get outputErrorUserName;
+
+  Stream<bool> get outputIsMobileNumberValid;
+  Stream<String> get outputErrorMobileNumber;
+
+  Stream<bool> get outputIsEmailValid;
+  Stream<String> get outputErrorEmail;
+
+  Stream<bool> get outputIsPasswordValid;
+  Stream<String> get outputErrorPassword;
+
+  Stream<File> get outputIsProfilePictureValid;
+  Stream<String> get outputErrorProfilePicture;
 
 }
