fdf580aaa695d2d22da6fd8c5fa21305f212b142
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-06-20 17:42:06 -0300

    Aula 142

--- lib/presentation/main/main_view.dart
+++ lib/presentation/main/main_view.dart
@@ -4,6 +4,8 @@ import 'package:curso_flutter_avancado/presentation/main/settings_page.dart';
 import 'package:curso_flutter_avancado/presentation/resources/strings_manager.dart';
 import 'package:flutter/material.dart';
 
+import '../resources/color_manager.dart';
+import '../resources/values_manager.dart';
 import 'notifications_pae.dart';
 
 class MainView extends StatefulWidget {
@@ -16,8 +18,12 @@ class MainView extends StatefulWidget {
 class _MainViewState extends State<MainView> {
 
   List<Widget> pages = [HomePage(),SearchPage(),NotificationsPage(),SettingsPage()];
-
-  var _title = AppStrings.home;
+  List<String> titles = [
+    AppStrings.home,
+    AppStrings.search,
+    AppStrings.notifications,
+    AppStrings.settings
+  ];
 
   var _currentIndex = 0;
 
@@ -25,11 +31,35 @@ class _MainViewState extends State<MainView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_title, style: Theme.of(context).textTheme.headline2,),
+        title: Text(titles[_currentIndex], style: Theme.of(context).textTheme.headline2,),
         centerTitle: true,
       ),
-      body: pages[_currentIndex]
+      body: pages[_currentIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(color: Colors.black, spreadRadius: AppSize.s1_5)
+          ]
+        ),
+        child: BottomNavigationBar(
+          selectedItemColor: ColorManager.primary,
+          unselectedItemColor: ColorManager.primary,
+          currentIndex: _currentIndex,
+          onTap: onTap,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.notifications), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.settings_outlined), label: ""),
+          ],
+        ),
+      ),
     );
+  }
+  onTap(int index){
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
 
