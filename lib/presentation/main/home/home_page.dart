d432be7efab3639f1c194f40412edb53fc6c61a1
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-06-26 15:43:19 -0300

    Aula 156

--- lib/presentation/main/home/home_page.dart
+++ lib/presentation/main/home/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:curso_flutter_avancado/presentation/main/home/home_viewmodel.dart';
+import 'package:curso_flutter_avancado/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
-
 import '../../../app/di.dart';
+import '../../common/state_renderer/state_render_impl.dart';
 import '../../resources/strings_manager.dart';
 
 class HomePage extends StatefulWidget {
@@ -28,9 +29,53 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(AppStrings.home),
+      child: SingleChildScrollView(
+        child: StreamBuilder<FlowState>(
+          stream: _viewModel.outputState,
+          builder: (context, snapshot){
+            return snapshot.data!.getScreenWidget(
+                context, _getContentWidget(), (){
+                  _viewModel.start();
+                }
+            ) ?? Container();
+          },
+        ),
+      ),
     );
   }
+
+  Widget _getContentWidget(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _getBannersCarousel(),
+        _getSection(AppStrings.services),
+        _getServices(),
+        _getSection(AppStrings.stores),
+        _getStores(),
+      ],
+    );
+  }
+
+  Widget _getBannersCarousel(){
+    return Container();
+  }
+
+  Widget _getSection(String title){
+    return Padding(
+      padding: EdgeInsets.only(top: AppPadding.p12, left: AppPadding.p12, right: AppPadding.p12, bottom: AppPadding.p2),
+      child: Text(title, style: Theme.of(context).textTheme.headline3,),
+    );
+  }
+
+  Widget _getServices(){
+    return Center();
+  }
+
+  Widget _getStores(){
+    return Center();
+  }
+
 
   @override
   void dispose() {
