41a67c2f98e06bd0b7be153388bed0c024c0bf47
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-06-26 14:55:24 -0300

    Aula 153

--- lib/presentation/main/home/home_viewmodel.dart
+++ lib/presentation/main/home/home_viewmodel.dart
@@ -4,7 +4,7 @@ import 'package:curso_flutter_avancado/presentation/base/baseviewmodel.dart';
 import '../../../domain/model/model.dart';
 import '../../../domain/usecase/home_usecase.dart';
 
-class HomeViewModel extends BaseViewModel{
+class HomeViewModel extends BaseViewModel with HomeViewModelInput, HomeViewModelOutput{
 
   StreamController _bannerStreamController = BehaviorSubject<List<Banner>>();
   StreamController _servicesStreamController = BehaviorSubject<List<Service>>();
@@ -23,7 +23,36 @@ class HomeViewModel extends BaseViewModel{
     _bannerStreamController.close();
     _servicesStreamController.close();
     _storeStreamController.close();
-    super.dispose();
   }
 
+  @override
+  Sink get inputBanners => _bannerStreamController.sink;
+
+  @override
+  Sink get inputServices => _servicesStreamController.sink;
+
+  @override
+  Sink get inputStores => _storeStreamController.sink;
+
+  @override
+  Stream<List<Banner>> get outputBanners => _bannerStreamController.stream.map((banners) => banners);
+
+  @override
+  Stream<List<Service>> get outputServices => _servicesStreamController.stream.map((services) => services);
+
+  @override
+  Stream<List<Store>> get outputStores => _storeStreamController.stream.map((stores) => stores);
+
+}
+
+abstract class HomeViewModelInput{
+  Sink get inputStores;
+  Sink get inputServices;
+  Sink get inputBanners;
+}
+
+abstract class HomeViewModelOutput{
+  Stream<List<Store>> get outputStores;
+  Stream<List<Service>> get outputServices;
+  Stream<List<Banner>> get outputBanners;
 }
